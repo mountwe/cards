28b0aebe22fb8343808323fffc4a85624a3073b1
Gene Mountjoy <gmountjoy@eh.local>
2019-09-06 13:01:43 -0400

    Add deck.go, modify main.go to utilize the print function in deck.go.

--- main.go
+++ main.go
@@ -1,15 +1,12 @@
 package main
 
-import "fmt"
+// import "fmt"
 
 func main() {
-    cards := []string{"Ace of Diamonds", newCard()}
+    cards := deck{"Ace of Diamonds", newCard()}
     cards = append(cards, "Six of Spades")
     
-    for i, card := range cards {
-        fmt.Println(i, card)
-    }
-}
+    cards.print()}
 
 func newCard() string {
     return "Five of Diamonds"
